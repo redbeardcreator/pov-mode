4dd7bdf8d215077435cd8539292cc5b78251a5a7
Michael D Johnson <redbeard@mdjohnson.us>
2014-03-19 21:52:36 -0500

    Include #for in pov-begin-re

--- pov-mode.el
+++ pov-mode.el
@@ -833,7 +833,7 @@ font-pov-keyword-face"
   "PoV hack to handle Emacs/XEmacs foo")
 
 (defvar pov-begin-re
-  "\\<#\\(if\\(n?def\\)?\\|case\\|macro\\|range\\|switch\\|while\\)\\>")
+  "\\<#\\(if\\(n?def\\)?\\|for\\|case\\|macro\\|range\\|switch\\|while\\)\\>")
 
 (defvar pov-end-re  "\\<#break\\|#end\\>")
 
